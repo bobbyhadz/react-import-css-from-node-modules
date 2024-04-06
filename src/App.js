da4e78209c276c5d571b7000b3c15ce8b2e98aad
bobbyhadz <opmftw@gmail.com>
2024-04-06 14:06:58 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// 👇️ import CSS from node_modules
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h2>bobbyhadz.com</h2>
+
+      <h2 className="text-white bg-dark">hello world</h2>
+    </div>
+  );
+}
+
+export default App;
